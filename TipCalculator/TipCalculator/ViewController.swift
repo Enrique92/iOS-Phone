9373742ae6a7f13de73440a1d2b89a2556346c12
Enrique <enrique.alcaide92@gmail.com>
2019-04-30 16:15:02 -0700

    Assignment5
    Assignment 5 iOS

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -11,27 +11,38 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var billAmountTextField: UITextField!
+    @IBOutlet var tipAmountTextField: UITextField!
+    @IBOutlet var billFinalAmountLabel: UILabel!
+    @IBOutlet var sliderTipAmount: UISlider!
     @IBOutlet var calculateTip: UIButton!
-    @IBOutlet var tipAmountLabel: UILabel!
-    @IBOutlet var tipPercentageTextField: UITextField!
+    @IBOutlet var mainStackView: UIStackView!
     
+    var result: Double = 0.0
+    var tip: Double = 0.0
+    var bill: Double = 0.0
     
-    @IBAction func calculateTip(_ sender: Any) {
-        var result: Double = 0.0
-        var tip: Double = 0.0
+    // Update the textField of the tip if we change the value of the slider
+    @IBAction func updateAmountTipWithSlider(_ sender: UISlider) {
+        tipAmountTextField.text = String(sender.value)
+    }
+    
+    @IBAction func calculateTheTip(_ sender: Any) {
         //let bill = billAmountTextField.text
         if let doubleValue = Double(billAmountTextField.text!) {
-            if (tipPercentageTextField.text != "") {
+            bill = doubleValue
+            
+            if (tipAmountTextField.text != "") {
                 // If the person write tip get it if not the default one
-                if let tipAmount = Double(tipPercentageTextField.text!) {
+                if let tipAmount = Double(tipAmountTextField.text!) {
                     tip = doubleValue * (tipAmount/100)
                     result = doubleValue + tip
+                    
                 } else {
                     result = 0.0
                     tip = 0.0
                     // Show the result in the Label
-                    tipAmountLabel.text = String(result)
-                    print("Not a valid number: \(tipPercentageTextField.text!)")
+                    billFinalAmountLabel.text = String(result)
+                    print("Not a valid number: \(tipAmountTextField.text!)")
                 }
             } else {
                 tip = doubleValue * 0.15
@@ -39,20 +50,57 @@ class ViewController: UIViewController {
             }
             
             // Show the result in the Label
-            tipAmountLabel.text = String(result)
+            billFinalAmountLabel.text = String(result)
         } else {
             result = 0.0
             tip = 0.0
             // Show the result in the Label
-            tipAmountLabel.text = String(result)
+            billFinalAmountLabel.text = String(result)
             print("Not a valid number: \(billAmountTextField.text!)")
+        }
+    }
+    
+    @objc func keywordWillChangeNotification(notification: Notification) {
+        
+        let keyboardRect = mainStackView.alignment.rawValue-100
+        
+        if notification.name == UIResponder.keyboardWillShowNotification ||
+           notification.name == UIResponder.keyboardWillChangeFrameNotification {
+            // Move the original view to see the amount of the final bill
+            view.frame.origin.y = CGFloat(keyboardRect)
+        } else {
+            // Move the original view to see the amount of the final bill
+            view.frame.origin.y = 0
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        billAmountTextField.delegate = self
+        tipAmountTextField.delegate = self
         
+        NotificationCenter.default.addObserver(self, selector: #selector(keywordWillChangeNotification(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keywordWillChangeNotification(notification:)), name: UIResponder.keyboardWillShowNotification , object: nil)
     }
     
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+}
+
+// Update the Label everytime it change
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if Double(textField.text!) != tip {
+            calculateTheTip((Any).self)
+        }
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
